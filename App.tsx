a18f32366e3b660791c474a81976e0f97065d435
Marcelo <marcelosoaresinc@gmail.com>
2023-07-07 14:55:03 -0300

    feat: google fonts

--- App.tsx
+++ App.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components/native';
 import defaultTheme from './src/global/styles/default'
 import { NavigationContainer } from '@react-navigation/native'
+import { useFonts, Poppins_400Regular, Poppins_900Black_Italic} from '@expo-google-fonts/poppins'
+import AppLoading from 'expo-app-loading';
 import AppRoutes from './src/routes/app.routes';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_900Black_Italic,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <NavigationContainer>
@@ -13,4 +23,3 @@ export default function App() {
     </ThemeProvider>
   );
 }
-
