df334fabfdd34fb441a88f04b21749982fdb274a
Marcelo <marcelosoaresinc@gmail.com>
2023-07-07 19:52:33 -0300

    feat: expo splash-screen

--- App.tsx
+++ App.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import defaultTheme from './src/global/styles/default';
-import { useFonts, Poppins_900Black_Italic } from '@expo-google-fonts/poppins';
-import AppLoading from 'expo-app-loading';
+import { useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
+import * as SplashScreen from 'expo-splash-screen'; // Importe o expo-splash-screen
 import AppRoutes from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Poppins_900Black_Italic,
+    Poppins_400Regular,
   });
 
+  useEffect(() => {
+    const hideSplashScreen = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        if (fontsLoaded) {
+          await SplashScreen.hideAsync();
+        }
+      } catch (error) {
+        console.warn(error);
+      }
+    };
+    hideSplashScreen();
+  }, [fontsLoaded]);
+
   if (!fontsLoaded) {
-    return <AppLoading />;
+    return null;
   }
 
   return (
