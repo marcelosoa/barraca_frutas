9e6d909baeeab4281c176d5779ab1b33e7118b0e
Marcelo <marcelosoaresinc@gmail.com>
2023-07-08 17:36:06 -0300

    redux: name

--- src/screens/RegisterSupplier/name/index.tsx
+++ src/screens/RegisterSupplier/name/index.tsx
@@ -1,34 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Container, ViewName, InputFormView } from "./styled";
 import InputFormComponent from "../../../components/InputForm";
 import ButtonComponent from "../../../components/Button";
 import { useNavigation } from "@react-navigation/native";
 import RouterComponent from "../../../components/Router";
+import { useDispatch, useSelector } from "react-redux";
+import { SupplierPropsState } from "../../../interface/SupplierInterface";
+import { setName } from "../../../redux/reducers/suppliersReducer";
 
 export default function RegisterNameSupplier() {
+  const navigation = useNavigation();
 
-  const navigation = useNavigation()
-
+  const { name } = useSelector(
+    (state: { supplier: SupplierPropsState }) => state.supplier
+  );
+  const dispatch = useDispatch();
+  
   return (
     <Container>
       <ViewName>
-        <Ionicons 
-          name="close" 
-          size={32} 
-          color={"#930000"} 
-        />
+        <Ionicons name="close" size={32} color={"#930000"} />
       </ViewName>
       <RouterComponent />
       <InputFormView>
-        <InputFormComponent 
+        <InputFormComponent
           label="Digite o Nome do colaborador"
-          onChange={() => {}}
-          placeholder="Nome"        
+          onChange={(value: string) => dispatch(setName(value))}
+          placeholder="Nome"
+          value={name}
         />
         <ButtonComponent
-          onPress={() => navigation.navigate('CPF')}
-          label="Próximo"        
+          onPress={() => navigation.navigate("CPF")}
+          label="Próximo"
         />
       </InputFormView>
     </Container>
