2d03aded2558df8ad0769b241f347bc262c0596e
Marcelo <marcelosoaresinc@gmail.com>
2023-07-08 17:36:22 -0300

    refactor: inputFormComponent

--- src/components/InputForm/index.tsx
+++ src/components/InputForm/index.tsx
@@ -4,19 +4,28 @@ import { TextInput, Container, Label, ErrorText } from './styled';
 interface InputProps {
   placeholder?: string;
   label?: string;
-  onChange?: (value: string) => void;
+  onChange?: (value: any) => void;
   limitCaracter?: number;
-  errors?: string; 
+  errors?: string;
+  value: string
 }
 
-export default function InputFormComponent({ placeholder, label, onChange, limitCaracter, errors }: InputProps) {
+export default function InputFormComponent({ placeholder, label, onChange, limitCaracter, errors, value = '' }: InputProps) {
+
+  const handleChange = (value: string) => {
+    if (onChange) {
+      onChange(value);
+    }
+  };
+
   return (
     <Container>
       <Label>{label}</Label>
       <TextInput 
           maxLength={limitCaracter} 
           placeholder={placeholder} 
-          onChangeText={onChange} 
+          onChangeText={handleChange} 
+          value={value}
       />
       {errors && <ErrorText>{errors}</ErrorText>}
     </Container>
