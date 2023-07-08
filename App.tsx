b466a160c5dfc86ef37a8e13f9f1c29c74b43d75
Marcelo <marcelosoaresinc@gmail.com>
2023-07-07 22:03:39 -0300

    feat: some fixs

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components/native';
 import defaultTheme from './src/global/styles/default';
-import { useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
+// import { useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
+import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen'; // Importe o expo-splash-screen
 import AppRoutes from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
-    Poppins_400Regular,
+    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
   });
 
   useEffect(() => {
