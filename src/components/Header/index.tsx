029f1691f3cc27e020ac2d3392f2a9164409546e
Marcelo <marcelosoaresinc@gmail.com>
2023-07-11 11:34:02 -0300

    new component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from './styled'
+
+export default function HeaderComponent () {
+  return (
+    <View>
+      
+    </View>
+  )
+}
---
+++ src/components/Header/styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native'
+
+
+export const Container
+
+export const View = styled.View``
