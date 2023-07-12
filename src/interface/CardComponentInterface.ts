26e80bb2a85d41fbd81827c0683066db91da461c
Marcelo <marcelosoaresinc@gmail.com>
2023-07-12 16:16:57 -0300

    feat: FRUITS REDUCER

--- src/interface/CardComponentInterface.ts
+++ src/interface/CardComponentInterface.ts
@@ -2,4 +2,7 @@ export interface CardComponentProps {
   cpf: string,
   name: string,
   phone: string
+  price?: string
+  quantity?: string
+  supplier?: string
 }
--- src/redux/reducers/fruitsReducer.ts
+++ src/redux/reducers/fruitsReducer.ts
@@ -1,4 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { v4 as uuidv4 } from 'uuid';
 export interface Fruit {
   id: string;
   name: string;
@@ -20,20 +22,43 @@ const fruitSlice = createSlice({
   initialState,
   reducers: {
     addFruit: (state, action: PayloadAction<Fruit>) => {
-      state.fruits.push(action.payload);
+      const newFruit: Fruit = {
+        ...action.payload,
+        id: uuidv4()
+      }
+      state.fruits.push(newFruit);
+      AsyncStorage.setItem('fruits', JSON.stringify(state.fruits))
     },
     removeFruit: (state, action: PayloadAction<string>) => {
       state.fruits = state.fruits.filter((fruit) => fruit.id !== action.payload);
+      AsyncStorage.setItem('fruits', JSON.stringify(state.fruits))
     },
     updateFruit: (state, action: PayloadAction<Fruit>) => {
       const index = state.fruits.findIndex((fruit) => fruit.id === action.payload.id);
       if (index !== -1) {
         state.fruits[index] = action.payload;
+        AsyncStorage.setItem('fruits', JSON.stringify(state.fruits))
       }
+    },
+    loadFruits: (state, action: PayloadAction<Fruit[]>) => {
+      state.fruits = action.payload;
     },
   },
 });
 
-export const { addFruit, removeFruit, updateFruit } = fruitSlice.actions;
+export const { addFruit, removeFruit, updateFruit, loadFruits } = fruitSlice.actions;
+
+export const fetchFruits = () => (dispatch: any) => {
+  AsyncStorage.getItem('fruits')
+    .then((fruitsData) => {
+      if (fruitsData) {
+        const fruits = JSON.parse(fruitsData);
+        dispatch(loadFruits(fruits));
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 export default fruitSlice.reducer;
