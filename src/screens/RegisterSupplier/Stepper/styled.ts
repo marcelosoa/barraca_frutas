4a55fef06dd024ba928c1975d18e8ef1cf11c559
Marcelo <marcelosoaresinc@gmail.com>
2023-07-12 23:49:04 -0300

    feat: stepper-90%

---
+++ src/screens/RegisterSupplier/Stepper/styled.ts
@@ -0,0 +1,40 @@
+import styled, { css } from 'styled-components/native'
+
+
+interface TextProps {
+  isRed?: boolean
+}
+
+export const Container = styled.SafeAreaView`
+  padding: 24px 28px;
+  display: flex;
+  flex: 1;
+  gap: 36px;
+`
+
+export const ViewName = styled.View`
+  flex-direction: row;
+  align-items: center;
+
+`
+export const InputFormView = styled.View`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: space-between;
+  align-items: center;
+`
+
+
+export const Text = styled.Text<TextProps>`
+  font-family: ${({ theme }) => theme.fonts.regular };
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 0.25px;
+  color: ${({ theme }) => theme.text.black};
+  ${({ isRed }) => isRed ? css`
+    color: ${({ theme }) => theme.colors.primary };
+  ` : css`
+    color: ${({ theme }) => theme.text.black };
+  `}
+`
