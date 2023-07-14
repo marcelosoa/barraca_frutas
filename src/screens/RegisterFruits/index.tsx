7c611e23e8fad422166a386bba0feb8b755be7df
Marcelo <marcelosoaresinc@gmail.com>
2023-07-14 11:10:13 -0300

    refactor: yarn

--- src/screens/RegisterFruits/index.tsx
+++ src/screens/RegisterFruits/index.tsx
@@ -9,22 +9,22 @@ import { useTheme } from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { addFruit } from "../../redux/reducers/fruitsReducer";
 import { RootState } from "../../redux/store";
-import { v4 as uuidv4 } from 'uuid';
-import RNPickerSelect from 'react-native-picker-select';
+import { v4 as uuidv4 } from "uuid";
+import { Picker } from "@react-native-picker/picker";
 
 export default function RegisterFruitScreen() {
   const navigation = useNavigation<propsStack>();
   const theme = useTheme();
   const dispatch = useDispatch();
-
-  const suppliers = useSelector((state: RootState) => state.supplier.suppliers)
+  const [selectSupplier, setSelectedSupplier] = useState();
+  const suppliers = useSelector((state: RootState) => state.supplier.suppliers);
 
   const [fruitData, setFruitData] = useState({
     name: "",
     price: "",
     quantity: "",
     supplier: "",
-    id: ''
+    id: "",
   });
 
   return (
@@ -39,43 +39,76 @@ export default function RegisterFruitScreen() {
       </ViewContent>
       <FruitView style={styled.card}>
         <Ionicons name="nutrition-outline" size={32} color={theme.text.black} />
-        <TextInput 
+        <TextInput
           placeholder="Nome da Fruta"
           value={fruitData.name}
-          onChangeText={(text: string) => setFruitData({ ...fruitData, name: text})}
-          />
+          onChangeText={(text: string) =>
+            setFruitData({ ...fruitData, name: text })
+          }
+        />
       </FruitView>
       <FruitView style={styled.card}>
         <Ionicons name="cash-outline" size={32} />
-        <TextInput 
+        <TextInput
           placeholder="Preço do Kilo"
           value={fruitData.price}
-          onChangeText={(price: string) => setFruitData({ ...fruitData, price: price})}
-          />
+          onChangeText={(price: string) =>
+            setFruitData({ ...fruitData, price: price })
+          }
+        />
       </FruitView>
       <FruitView style={styled.card}>
         <Ionicons name="server-outline" size={32} />
-        <TextInput 
+        <TextInput
           placeholder="Quantidade no estoque"
           value={fruitData.quantity}
-          onChangeText={(quantity: string) => setFruitData({ ...fruitData, quantity: quantity})}
+          onChangeText={(quantity: string) =>
+            setFruitData({ ...fruitData, quantity: quantity })
+          }
         />
       </FruitView>
       <FruitView style={styled.card}>
         <Ionicons name="people-outline" size={32} />
-          <RNPickerSelect
-            onValueChange={(supplier: string) => setFruitData({...fruitData, supplier: supplier})}
-            items={suppliers.map((supplier) => (
-              { label: supplier.name, value: supplier.name }
-            ))}
-          />
+        {/* <RNPickerSelect
+          placeholder={{
+            label: "Fornecedor",
+            value: null,
+            color: "#9EA0A4",
+          }}
+          onValueChange={(supplier: string) =>
+            setFruitData({ ...fruitData, supplier: supplier })
+          }
+          items={suppliers.map((supplier) => ({
+            label: supplier.name,
+            value: supplier.name,
+          }))}
+          style={{
+            inputIOS: styled.picker,
+            inputAndroid: styled.picker,
+          }}
+        /> */}
+        {/* <Picker
+          selectedValue={fruitData.supplier}
+          onValueChange={(supplier: string) =>
+            setFruitData({ ...fruitData, supplier: supplier })
+          }
+        >
+          <Picker.Item label="Fornecedor" value="" />
+          {suppliers.map((supplier) => (
+            <Picker.Item
+              key={supplier.name}
+              label={supplier.name}
+              value={supplier.name}
+            />
+          ))}
+        </Picker> */}
       </FruitView>
       <ButtonComponent
         label="Cadastrar Fruta"
         onPress={() => {
-          fruitData.id = uuidv4()
+          fruitData.id = uuidv4();
           dispatch(addFruit(fruitData));
-          navigation.navigate('SuccessFruit');
+          navigation.navigate("SuccessFruit");
         }}
       />
     </Container>
@@ -88,18 +121,18 @@ const styled = StyleSheet.create({
     flexDirection: "row",
     gap: 16,
     padding: 16,
-    elevation: 2,
     backgroundColor: "#FFFFFF",
+    elevation: 1,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
     borderRadius: 8,
-    shadowColor: "black",
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.6,
-    shadowRadius: 6,
   },
   picker: {
-    backgroundColor: 'black'
-  }
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
 });
