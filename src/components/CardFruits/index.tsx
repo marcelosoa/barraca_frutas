795ecf2e9abbf978ec90e356d6322486bd3bc177
Marcelo <marcelosoaresinc@gmail.com>
2023-07-13 23:45:10 -0300

    feat: fruits creen done

--- src/components/CardFruits/index.tsx
+++ src/components/CardFruits/index.tsx
@@ -1,8 +1,15 @@
 import React, { useState } from "react";
 import { CardFruitProps } from "../../interface/card_fruits.interface";
-import { Container, ContainerCard, View, Text, TextName, PriceContainer, ViewSupplier } from "./styled";
+import {
+  Container,
+  ContainerCard,
+  View,
+  Text,
+  TextName,
+  PriceContainer,
+  ViewSupplier,
+} from "./styled";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { StyleSheet } from "react-native";
 import { useTheme } from "styled-components";
 import { useNavigation } from "@react-navigation/native";
 import { propsStack } from "../../interface/routerinterface";
@@ -13,21 +20,24 @@ export default function CardFruitComponent({
   quantity,
   supplier,
 }: CardFruitProps) {
-  const theme = useTheme()
-  const [isOpenModal, setIsOpenModal] = useState<boolean>(false)
-  const navigation = useNavigation<propsStack>()
-  
+  const theme = useTheme();
+  const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const navigation = useNavigation<propsStack>();
+
   const isModalOpen = () => {
-    setIsOpenModal(true)
-  }
+    setIsOpenModal(true);
+  };
 
   return (
     <Container>
       <ContainerCard>
-        <View>
-        <Ionicons name="cog-outline" size={24} color={theme.colors.primary} onPress={() => isModalOpen}/>
-          <TextName>{name}</TextName>
-        </View>
+      <TextName>{name}</TextName>
+        <Ionicons
+          name="cog-outline"
+          size={24}
+          color={theme.colors.primary}
+          onPress={() => isModalOpen()}
+        />
       </ContainerCard>
       <PriceContainer>
         <Ionicons name="cash-outline" size={24} color={"#008C21"} />
@@ -41,35 +51,3 @@ export default function CardFruitComponent({
     </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  header: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  iconContainer: {
-    marginRight: 8,
-  },
-  nameContainer: {},
-  name: {
-    textAlign: "left",
-  },
-  priceContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  price: {
-    marginLeft: 4,
-  },
-  infoContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  supplier: {
-    marginLeft: 4,
-    marginRight: 4,
-  },
-  quantify: {
-    marginLeft: 4,
-  },
-});
--- src/components/CardFruits/styled.ts
+++ src/components/CardFruits/styled.ts
@@ -1,45 +1,45 @@
-import styled from 'styled-components/native'
+import styled from "styled-components/native";
 
 interface TextProps {
-  left?: number
+  left?: number;
 }
 
 export const Container = styled.View`
-  background-color: ${({ theme }) => theme.colors.shape };
-  padding: 16px;
+  background-color: ${({ theme }) => theme.colors.shape};
+  padding: 20px;
   border-radius: 8px;
-`
+`;
 
 export const ContainerCard = styled.View`
   flex-direction: row;
   align-items: center;
-`
+  justify-content: space-between;
+`;
 
 export const TextName = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.medium };
+  font-family: ${({ theme }) => theme.fonts.medium};
   font-size: 16px;
-`
+`;
 
 export const Text = styled.Text<TextProps>`
   display: flex;
   font-size: 13px;
-  margin-left: ${({ left }) => left ? left + 'px'  :  0};
-  font-family: ${({ theme }) => theme.fonts.regular };
-`
+  margin-left: ${({ left }) => (left ? left + "px" : 0)};
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
 
 export const PriceContainer = styled.View`
   flex-direction: row;
   align-items: center;
-`
+`;
 
 export const View = styled.View`
   display: flex;
-  justify-content: space-between;
-`
+  flex-direction: row;
+  align-items: center;
+`;
 
 export const ViewSupplier = styled.View`
   flex-direction: row;
   align-items: center;
-`
-
-export const ViewInfos = styled.View``
+`;
